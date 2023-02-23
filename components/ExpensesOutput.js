beb378bd6e076ed42c7f6c7194ec4bb57c9bb5f1
caw <dominik.wozniewicz@gmail.com>
2023-02-24 00:02:56 +0100

    dummy expenses

--- components/ExpensesOutput.js
+++ components/ExpensesOutput.js
@@ -2,10 +2,25 @@ import { View } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'A pair of shoes',
+    amount: 59.99,
+    date: new Date('2021-12-19'),
+  },
+  {
+    id: 'e2',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2021-07-23'),
+  },
+];
+
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,8 @@
 import { Text } from 'react-native';
+import ExpensesOutput from '../components/ExpensesOutput';
 
 const RecentExpenses = () => {
-  return <Text>RecentExpenses Screen</Text>;
+  return <ExpensesOutput expensesPeriod="Last 7 days" />;
 };
 
 export default RecentExpenses;
