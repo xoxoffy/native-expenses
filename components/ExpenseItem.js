e765fede5df6251630c868c6a909d2875b973388
caw <dominik.wozniewicz@gmail.com>
2023-03-04 23:18:39 +0100

    ExpenseItem

---
+++ components/ExpenseItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+
+const ExpenseItem = () => {
+  return <Pressable>ExpenseItem</Pressable>;
+};
+
+export default ExpenseItem;
