925c8362f5e4d92abf7148eaffd0ced4b515eef5
caw <dominik.wozniewicz@gmail.com>
2023-03-09 19:36:45 +0100

    Markup

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -2,7 +2,19 @@ import React from 'react';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const ExpenseItem = () => {
-  return <Pressable>ExpenseItem</Pressable>;
+  return (
+    <Pressable>
+      <View>
+        <View>
+          <Text> DESCRIPTION </Text>
+          <Text> DATE </Text>
+        </View>
+        <View>
+          <Text>AMOUNT</Text>
+        </View>
+      </View>
+    </Pressable>
+  );
 };
 
 export default ExpenseItem;
